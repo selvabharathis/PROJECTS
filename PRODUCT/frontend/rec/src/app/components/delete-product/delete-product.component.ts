2ad85237f517ab1ad478c3e1aec56dfb634f785d
selvabharathi <selvabharathi327@gmail.com>
2021-10-04 01:17:37 +0530

    feat(DeleteProduct)

--- PRODUCT/frontend/rec/src/app/components/delete-product/delete-product.component.ts
+++ PRODUCT/frontend/rec/src/app/components/delete-product/delete-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-delete-product',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteProductComponent implements OnInit {
 
-  constructor() { }
+  productId:number;
+
+  constructor(private apiService:ApiService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.apiService.deleteProduct(this.productId).subscribe(successMessage=>{
+      console.log(successMessage);
+    },errorMessage=>{
+      console.log(errorMessage);
+    })
   }
 
 }
